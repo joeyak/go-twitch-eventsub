f6c80d1e2c3b7452466745b8a4b62e6dfa0b3028
joeyak <joeyak97@gmail.com>
2022-12-18 12:16:03 -0500

    When the websocket client errors with context.Cancelled cleanly exit

--- conn.go
+++ conn.go
@@ -125,7 +125,7 @@ func (c *Client) ConnectWithContext(ctx context.Context) error {
 		_, data, err := c.ws.Read(ctx)
 		if err != nil {
 			var closeError websocket.CloseError
-			if c.closed && errors.As(err, &closeError) {
+			if c.closed && (errors.As(err, &closeError) || errors.Is(err, context.Canceled)) {
 				return nil
 			}
 			return fmt.Errorf("could not read message: %w", err)
